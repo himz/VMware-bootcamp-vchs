717e2e975861f2973e5bfcffcf9931f30bd7b9a6
Varun S Lingaraju <warunsl@gmail.com>
2014-08-18 16:16:23 -0700

    Add skeleton code for routes

--- Visualizations/viz.py
+++ Visualizations/viz.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask import render_template
 
+from mongo_utils import get_records
+
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('tl_index.html')
+
+
+@app.route("/apis")
+def apis():
+    pass
+
+
+@app.route("/requests")
+def servers():
+    pass
+
+
+@app.route("/clients")
+def clients():
+    pass
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
