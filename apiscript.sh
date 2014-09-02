bf54e1cace6c057da67fc0c4379f1de360fcb0cc
Prashant Gadagi <gadagip@changeme-mac-58.local>
2014-09-02 16:40:22 -0700

    modified the script

--- apiscript.sh
+++ apiscript.sh
@@ -9,36 +9,36 @@ then
 	echo "Windows - OS1, MacOS - OS2, Linux - OS3"
 else
 	$ua = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36";
-	if [ $7 -eq "B1" ]
+	if [ "$7" == "B1" ]
 	then
-		if [$8 -eq "OS2" ]
+		if [ "$8" == "OS2" ]
 		then
 			$ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36";
-		elif [$8 -eq "OS3" ]
+		elif [ "$8" == "OS3" ]
 		then
 			$ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1636.0 Safari/537.36";
 		fi
-	elif [ $7 -eq "B2" ]
+	elif [ "$7" == "B2" ]
         then
-                if [ $8 -eq "OS1" ]
+                if [ "$8" == "OS1" ]
                 then
 			$ua = "Mozilla/5.0 (Windows NT 6.1; rv:23.0) Gecko/20100101 Firefox/23.0";
-                elif [$8 -eq "OS2" ]
+                elif [ "$8" == "OS2" ]
                 then
 			$ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:23.0) Gecko/20100101 Firefox/23.0";
-                elif [$8 -eq "OS3" ]
+                elif [ "$8" == "OS3" ]
                 then
 			$ua = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:24.0) Gecko/20100101 Firefox/24.0";
                 fi
-        elif [ $7 -eq "B3" ]
+        elif [ "$7" == "B3" ]
         then
-                if [ $8 -eq "OS1" ]
+                if [ "$8" == "OS1" ]
                 then
 			$ua = "Mozilla/5.0 (Windows; U; Windows NT 6.1; tr-TR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27";
-                elif [$8 -eq "OS2" ]
+                elif [ "$8" == "OS2" ]
                 then
 			$ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.59.8 (KHTML, like Gecko) Version/5.1.9 Safari/534.59.8";
-                elif [$8 -eq "OS3" ]
+                elif [ "$8" == "OS3" ]
                 then
 			$ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.32 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.32 Epiphany/3.8.2";
                 fi
