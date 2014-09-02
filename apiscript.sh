2bc4d1ae9fc1a2521723d87fc942f4bc2975f449
Prashant Gadagi <gadagip@changeme-mac-58.local>
2014-09-02 16:13:04 -0700

    Modified scripts

--- apiscript.sh
+++ apiscript.sh
@@ -10,7 +10,7 @@ else
 	echo $url
 	while [ $count -lt $6 ]
 	do
-		curl -i -H "Accept: application/json" -X $5 $url > log.txt
+		curl -i -H "Accept: application/json" --user-agent "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36" -X $5 $url > log.txt
 		echo " "
 		sleep 0.025;
 		((count+=1));
